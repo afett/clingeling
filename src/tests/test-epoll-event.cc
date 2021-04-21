11b49b70735943f072895b9cdbea641f25c48443
Andreas Fett <a.fett@gmx.de>
2021-04-21 21:08:12 +0200

    epoll/event: add unit test

--- src/tests/test-epoll-event.cc
+++ src/tests/test-epoll-event.cc
@@ -16,12 +16,14 @@ private:
 	void operator_bool_test();
 	void operator_bit_and_test();
 	void operator_bit_or_test();
+	void clingeling_test();
 
 	CPPUNIT_TEST_SUITE(test);
 	CPPUNIT_TEST(default_ctor_test);
 	CPPUNIT_TEST(operator_bool_test);
 	CPPUNIT_TEST(operator_bit_and_test);
 	CPPUNIT_TEST(operator_bit_or_test);
+	CPPUNIT_TEST(clingeling_test);
 	CPPUNIT_TEST_SUITE_END();
 };
 
@@ -79,6 +81,21 @@ void test::operator_bit_or_test()
 		EPoll::Event ev{EPoll::Event::Type::In};
 		CPPUNIT_ASSERT(ev & EPoll::Event::Type::In);
 	}
+}
+
+void test::clingeling_test()
+{
+	EPoll::Event ev{EPoll::Event::Type::In};
+	ev = EPoll::Event::Type::Out;
+
+	if (ev & EPoll::Event::Type::Hup) {
+		CPPUNIT_ASSERT(false);
+	}
+	if (ev & EPoll::Event::Type::In) {
+		CPPUNIT_ASSERT(false);
+	}
+	CPPUNIT_ASSERT(ev & EPoll::Event::Type::Out);
+	CPPUNIT_ASSERT(!(ev & EPoll::Event::Type::In));
 }
 
 }}
