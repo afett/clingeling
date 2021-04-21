f504c42c6e9f6224fd39910c13c90fca2ec638d8
Andreas Fett <a.fett@gmx.de>
2021-04-21 21:06:35 +0200

    io/buffer: add read and write interface

--- src/include/io/buffer.h
+++ src/include/io/buffer.h
@@ -16,7 +16,28 @@
 
 namespace IO {
 
-class Buffer {
+class ReadBuffer {
+public:
+	virtual void *rstart() const = 0;
+	virtual size_t rsize() const = 0;
+	virtual void drain(size_t) = 0;
+	virtual bool empty() const = 0;
+
+	virtual ~ReadBuffer() = default;
+};
+
+class WriteBuffer {
+public:
+	virtual void *wstart() const = 0;
+	virtual size_t wsize() const = 0;
+	virtual void reserve(size_t) = 0;
+	virtual void fill(size_t) = 0;
+	virtual bool full() const = 0;
+
+	virtual ~WriteBuffer() = default;
+};
+
+class Buffer : public ReadBuffer, public WriteBuffer {
 public:
 	/* create a new iobuf */
 	Buffer() = default;
@@ -25,7 +46,7 @@ public:
 	explicit Buffer(size_t);
 
 	/* free iobuf and it's data */
-	~Buffer();
+	~Buffer() final;
 
 	/* call reserve before writing at most iobuf_wsize() bytes
 	 * starting at iobuf_wstart()
@@ -33,7 +54,7 @@ public:
 	 * this will invalidate any results from previous calls to
 	 * rsize, rstart, wsize, wstart
 	 */
-	void reserve(size_t);
+	void reserve(size_t) final;
 
 	/* call fill after writing at most iobuf_wsize() bytes
 	 * starting at iobuf_wstart()
@@ -41,15 +62,15 @@ public:
 	 * this will invalidate any results from previous calls to
 	 * rsize and wsize
 	 */
-	void fill(size_t);
+	void fill(size_t) final;
 
 	/* maximum size that may be read from rstart, and passed
 	 * to drain
 	 */
-	size_t rsize() const;
+	size_t rsize() const final;
 
 	/* start of data to read */
-	void *rstart() const;
+	void *rstart() const final;
 
 	/* call drain after you read at most iobuf_rsize() bytes
 	 * starting at iobuf_rstart()
@@ -57,16 +78,16 @@ public:
 	 * this will invalidate any results from previous calls to
 	 * rsize and wsize
 	 */
-	void drain(size_t);
+	void drain(size_t) final;
 
 	/* maximum size that may be written to wstart */
-	size_t wsize() const;
+	size_t wsize() const final;
 
 	/* start of data to write */
-	void *wstart() const;
+	void *wstart() const final;
 
-	bool empty() const;
-	bool full() const;
+	bool empty() const final;
+	bool full() const final;
 
 private:
 	void grow(size_t);
