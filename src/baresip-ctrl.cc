1d8a3f570592ffeacf3ceff0b7aa546bafb2d693
Andreas Fett <a.fett@gmx.de>
2021-05-16 18:19:59 +0200

    clingeling: move Baresip::Ctrl implementation

---
+++ src/baresip-ctrl.cc
@@ -0,0 +1,123 @@
+/*
+   Copyright (c) 2021 Andreas Fett. All rights reserved.
+   Use of this source code is governed by a BSD-style
+   license that can be found in the LICENSE file.
+*/
+#include "baresip/ctrl.h"
+
+#include "json/parser.h"
+#include "netstring/reader.h"
+#include "io/stream-buffer.h"
+#include "io/event-buffer.h"
+
+#include <sstream>
+
+namespace Baresip {
+
+class CtrlImpl : public Ctrl {
+public:
+	explicit CtrlImpl(IO::ReadEventBuffer &, IO::WriteBuffer &);
+	void on_event(std::function<void(Event const&)> const&) final;
+
+private:
+	void on_json(Json::Object const&);
+
+	IO::StreamBuffer recvbuf_;
+	Netstring::Reader netstring_;
+	IO::WriteBuffer & sendbuf_;
+	std::function<void(Event const&)> on_event_;
+};
+
+std::unique_ptr<Ctrl> Ctrl::create(IO::ReadEventBuffer & recvbuf, IO::WriteBuffer & sendbuf)
+{
+	return std::make_unique<CtrlImpl>(recvbuf, sendbuf);
+}
+
+CtrlImpl::CtrlImpl(IO::ReadEventBuffer & recvbuf, IO::WriteBuffer & sendbuf)
+:
+	recvbuf_(recvbuf),
+	netstring_(recvbuf_),
+	sendbuf_(sendbuf)
+{
+	recvbuf.on_fill([this] () {
+		auto data = std::string{};
+		if (netstring_.parse(data)) {
+			std::stringstream ss{data};
+			on_json(Json::parse_object(ss));
+		}
+	});
+}
+
+void CtrlImpl::on_event(std::function<void(Event const&)> const& cb)
+{
+	on_event_ = cb;
+}
+
+namespace {
+
+Event::Type event_type(std::string const&)
+{
+	return Event::Type::RegisterFail;
+}
+
+Event::Class event_class(std::string const&)
+{
+	return Event::Class::Register;
+}
+
+template <typename T>
+std::add_pointer_t<const T> get_if(Json::Object const* obj, const char* name)
+{
+	auto member = obj->find(std::string(name));
+	if (member == std::end(*obj)) {
+		return nullptr;
+	}
+
+	return Json::get_if<T>(&member->second);
+}
+
+bool is_event(Json::Object const& obj)
+{
+	if (auto event = get_if<bool>(&obj, "event")) {
+		return *event;
+	}
+	return false;
+}
+
+}
+
+void CtrlImpl::on_json(Json::Object const& obj)
+{
+	if (!is_event(obj)) {
+		return;
+	}
+
+	Event ev;
+	if (auto type_str = get_if<std::string>(&obj, "type")) {
+		ev.type = event_type(*type_str);
+	} else {
+		throw std::runtime_error("failed to get event type");
+	}
+
+	if (auto class_str = get_if<std::string>(&obj, "class")) {
+		ev.klass = event_class(*class_str);
+	} else {
+		throw std::runtime_error("failed to get event class");
+	}
+
+	if (auto accountaor = get_if<std::string>(&obj, "accountaor")) {
+		ev.accountaor = *accountaor;
+	} else {
+		throw std::runtime_error("failed to get event accountaor");
+	}
+
+	if (auto param = get_if<std::string>(&obj, "param")) {
+		ev.param = *param;
+	} else {
+		throw std::runtime_error("failed to get event param");
+	}
+
+	on_event_(ev);
+}
+
+}
--- src/clingeling.cc
+++ src/clingeling.cc
@@ -7,16 +7,12 @@
 #include "posix/socket.h"
 #include "posix/inet-address.h"
 #include "posix/socket-address.h"
-#include "io/stream-buffer.h"
 #include "posix/pipe-factory.h"
 #include "buffered-stream-socket.h"
-#include "netstring/reader.h"
-
-#include "json/parser.h"
+#include "baresip/ctrl.h"
 
 #include <system_error>
 #include <iostream>
-#include <sstream>
 
 std::ostream & operator<<(std::ostream & os, Posix::StreamSocket::State state)
 {
@@ -29,117 +25,6 @@ std::ostream & operator<<(std::ostream & os, Posix::StreamSocket::State state)
 	return os;
 }
 
-namespace Baresip {
-class Event {
-public:
-	enum class Type {
-		RegisterFail,
-	};
-
-	enum class Class {
-		Register,
-	};
-
-	Type type;
-	Class klass;
-	std::string accountaor;
-	std::string param;
-};
-
-Event::Type event_type(std::string const&)
-{
-	return Event::Type::RegisterFail;
-}
-
-Event::Class event_class(std::string const&)
-{
-	return Event::Class::Register;
-}
-
-template <typename T>
-std::add_pointer_t<const T> get_if(Json::Object const* obj, const char* name)
-{
-	auto member = obj->find(std::string(name));
-	if (member == std::end(*obj)) {
-		return nullptr;
-	}
-
-	return Json::get_if<T>(&member->second);
-}
-
-bool is_event(Json::Object const& obj)
-{
-	if (auto event = get_if<bool>(&obj, "event")) {
-		return *event;
-	}
-	return false;
-}
-
-class Ctrl {
-public:
-	explicit Ctrl(IO::ReadEventBuffer & recvbuf, IO::WriteBuffer & sendbuf)
-	:
-		recvbuf_(recvbuf),
-		netstring_(recvbuf_),
-		sendbuf_(sendbuf)
-	{
-		recvbuf.on_fill([this] () {
-			auto data = std::string{};
-			if (netstring_.parse(data)) {
-				std::stringstream ss{data};
-				on_json(Json::parse_object(ss));
-			}
-		});
-	}
-
-	void on_event(std::function<void(Event const&)> const& cb)
-	{
-		on_event_ = cb;
-	}
-
-private:
-	void on_json(Json::Object const& obj)
-	{
-		if (!is_event(obj)) {
-			return;
-		}
-
-		Event ev;
-		if (auto type_str = get_if<std::string>(&obj, "type")) {
-			ev.type = event_type(*type_str);
-		} else {
-			throw std::runtime_error("failed to get event type");
-		}
-
-		if (auto class_str = get_if<std::string>(&obj, "class")) {
-			ev.klass = event_class(*class_str);
-		} else {
-			throw std::runtime_error("failed to get event class");
-		}
-
-		if (auto accountaor = get_if<std::string>(&obj, "accountaor")) {
-			ev.accountaor = *accountaor;
-		} else {
-			throw std::runtime_error("failed to get event accountaor");
-		}
-
-		if (auto param = get_if<std::string>(&obj, "param")) {
-			ev.param = *param;
-		} else {
-			throw std::runtime_error("failed to get event param");
-		}
-
-		on_event_(ev);
-	}
-
-	IO::StreamBuffer recvbuf_;
-	Netstring::Reader netstring_;
-	IO::WriteBuffer & sendbuf_;
-	std::function<void(Event const&)> on_event_;
-};
-
-}
-
 int clingeling(int, char *[])
 {
 	auto poller_factory = EPoll::CtrlFactory::create();
@@ -150,7 +35,7 @@ int clingeling(int, char *[])
 	auto socket_buffer = BufferedStreamSocket(
 		*poller, *socket_factory, Posix::SocketAddress{Posix::Inet::Address{"127.0.0.1"}, 4444});
 
-	auto baresip_ctrl = Baresip::Ctrl{socket_buffer.recvbuf(), socket_buffer.sendbuf()};
+	auto baresip_ctrl = Baresip::Ctrl::create(socket_buffer.recvbuf(), socket_buffer.sendbuf());
 
 	auto pipe_factory = Posix::PipeFactory::create();
 	auto pipe = pipe_factory->make_pipe(Posix::PipeFactory::Params{false, true});
