b2e632f5aa64b36f6b207d1a4abc51538e0e2a82
Andreas Fett <a.fett@gmx.de>
2021-05-25 21:43:35 +0200

    clingeling: add more debug output

--- src/clingeling.cc
+++ src/clingeling.cc
@@ -4,6 +4,7 @@
    license that can be found in the LICENSE file.
 */
 #include "epoll/ctrl.h"
+#include "fmt.h"
 #include "posix/socket.h"
 #include "posix/inet-address.h"
 #include "posix/socket-address.h"
@@ -25,6 +26,52 @@ std::ostream & operator<<(std::ostream & os, Posix::StreamSocket::State state)
 	return os;
 }
 
+std::ostream & operator<<(std::ostream & os, Baresip::Event::Register::Type type)
+{
+	switch (type) {
+	case Baresip::Event::Register::Type::Fail: return os << "Fail";
+	case Baresip::Event::Register::Type::Ok:  return os << "Ok";
+	case Baresip::Event::Register::Type::Unregistering: return os << "Unregistering";
+	}
+	return os;
+}
+
+std::ostream & operator<<(std::ostream & os, Baresip::Event::Call::Type type)
+{
+	switch (type) {
+	case Baresip::Event::Call::Type::Established: return os << "Established";
+	case Baresip::Event::Call::Type::Incoming: return os << "Incoming";
+	case Baresip::Event::Call::Type::Ringing: return os << "Ringing";
+	case Baresip::Event::Call::Type::Closed: return os << "Closed";
+	}
+	return os;
+}
+
+std::ostream & operator<<(std::ostream & os, Baresip::Event::Call::Direction direction)
+{
+	switch (direction) {
+	case Baresip::Event::Call::Direction::Incoming: return os << "Incoming";
+	case Baresip::Event::Call::Direction::Outgoing: return os << "Outgoing";
+	}
+	return os;
+}
+
+namespace {
+
+struct EventLogger {
+	void operator()(Baresip::Event::Register const& ev)
+	{
+		std::cerr << Fmt::format("Event: Register: %s, %s, '%s'", ev.type, ev.accountaor, ev.param) << "\n";
+	}
+
+	void operator()(Baresip::Event::Call const& ev)
+	{
+		std::cerr << Fmt::format("Event: Call: %s, %s, %s, %s, %s, '%s'", ev.type, ev.accountaor, ev.direction, ev.peeruri, ev.id, ev.param) << "\n";
+	}
+};
+
+}
+
 int clingeling(int, char *[])
 {
 	auto poller_factory = EPoll::CtrlFactory::create();
@@ -36,6 +83,8 @@ int clingeling(int, char *[])
 		*poller, *socket_factory, Posix::SocketAddress{Posix::Inet::Address{"127.0.0.1"}, 4444});
 
 	auto baresip_ctrl = Baresip::Ctrl::create(socket_buffer.recvbuf(), socket_buffer.sendbuf());
+	baresip_ctrl->on_event([](auto ev) { std::visit(::EventLogger{}, ev); });
+	baresip_ctrl->on_response([](auto const&) { std::cerr << "unexpected response object\n"; });
 
 	auto pipe_factory = Posix::PipeFactory::create();
 	auto pipe = pipe_factory->make_pipe(Posix::PipeFactory::Params{false, true});
