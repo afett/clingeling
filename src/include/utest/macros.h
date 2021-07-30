4fb791af24a8b30950b606865bb6ab4da9fe5f25
Andreas Fett <a.fett@gmx.de>
2021-07-30 20:19:04 +0200

    utest: fix macro indent

--- src/include/utest/macros.h
+++ src/include/utest/macros.h
@@ -36,25 +36,26 @@
 #define UTEST_ASSERT_EQUAL(expected, result) \
 	UTest::assert_equal((expected), (result), std::string{ #expected }, std::string{ #result }, SourceLocation::current())
 
-#define UTEST_ASSERT_THROW(expr, exception) \
-	try {   auto utest_failure_location = SourceLocation::current();         \
-		(expr);                                                     \
+#define UTEST_ASSERT_THROW(expr, exception)                              \
+	try {                                                            \
+		auto utest_failure_location = SourceLocation::current(); \
+		(expr);                                                  \
 		throw UTest::AssertionFailure{std::string("Expected ") + #expr + " to throw exception of type " + #exception, utest_failure_location}; \
-	} catch (exception const& e) {                                      \
+	} catch (exception const& e) {                                   \
 	}
 
-#define UTEST_CASE(name)                                        \
-class name {                                                   \
-public:                                                        \
-	void operator()();                                     \
-};                                                             \
+#define UTEST_CASE(name)   \
+class name {               \
+public:                    \
+	void operator()(); \
+};                         \
 auto name ## Registrator = UTest::Registrator<name>{SourceLocation::current()}; \
 void name::operator()()
 
-#define UTEST_CASE_WITH_FIXTURE(name, fixture)                  \
-class name : public fixture {                                  \
-public:                                                        \
-	void operator()();                                     \
-};                                                             \
+#define UTEST_CASE_WITH_FIXTURE(name, fixture) \
+class name : public fixture {                  \
+public:                                        \
+	void operator()();                     \
+};                                             \
 auto name ## Registrator = UTest::Registrator<name>{SourceLocation::current()};  \
 void name::operator()()
