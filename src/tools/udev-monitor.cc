4781b25dffa2a23f2ea4d2d773413975ce6c35df
Andreas Fett <a.fett@gmx.de>
2021-09-20 21:09:41 +0200

    tools/udev-monitor: dump kobject_uevent messages

--- src/tools/udev-monitor.cc
+++ src/tools/udev-monitor.cc
@@ -2,13 +2,53 @@
 #include "source-location.h"
 
 #include "netlink/socket.h"
+#include "posix/system-error.h"
+
+#include <iostream>
+#include <cctype>
 
 namespace {
 
-void on_netlink_event(EPoll::Events const& ev)
+void on_netlink_event(EPoll::Events const& ev, std::shared_ptr<Netlink::Socket> const& socket)
 {
 	if (ev != EPoll::Event::In) {
-		throw std::runtime_error("bad epoll event");
+		auto ec = socket->get_socket_error();
+		throw Posix::make_system_error(ec.value(), "bad epoll event");
+	}
+
+	auto buf = std::array<char, 8192>{};
+	auto size = socket->read(buf.data(), buf.size());
+
+	if (size == buf.size() || size < 5) {
+		return;
+	}
+
+	auto end = std::begin(buf) + size;
+	if (*std::prev(end) != '\0') {
+		return;
+	}
+
+	// bind@/devices/pci0000:00/0000:00:14.0/usb1/1-2/1-2:1.0.ACTION=bind.DEVPATH=/devices/pci0000:00/0000:00:14.0/usb1/1-2/1-2:1.0.SUBSYSTEM=usb.DEVTYPE=usb_interface.DRIVER=usbhid.PRODUCT=b0e/e44/120.TYPE=0/0/0.INTERFACE=3/0/0.MODALIAS=usb:v0B0Ep0E44d0120dc00dsc00dp00ic03isc00ip00in00.SEQNUM=4424.
+
+	for (auto i{0U}; i < size; ++i) {
+		if (buf[i] == '\0') {
+			buf[i] = '\n';
+		} else if (!std::isprint(buf[i])) {
+			buf[i] = '.';
+		}
+	}
+	std::cerr << "\n----- MESSAGE BEGIN -----\n";
+	std::cerr << buf.data();
+	std::cerr << "----- MESSAGE END -----\n";
+}
+
+void backtrace(std::exception const& e)
+{
+	std::cerr << e.what() << "\n";
+	try {
+		std::rethrow_if_nested(e);
+	} catch (std::exception const& e) {
+		backtrace(e);
 	}
 }
 
@@ -21,18 +61,20 @@ int main()
 
 	auto netlink_socket_factory = Netlink::SocketFactory::create();
 	auto netlink_socket = netlink_socket_factory->make_socket(
-			Netlink::Family::KObjectUEvent, {});
-	netlink_socket->bind(0x0002);
+			Netlink::Family::KObjectUEvent, {Posix::Fd::Option::nonblock|Posix::Fd::Option::cloexec});
+	netlink_socket->set_recvbuf(128*1024*1024);
+	netlink_socket->bind(0x0001);
 
 	poller->add(netlink_socket, EPoll::Events{EPoll::Event::In},
-			[](auto const& ev) { ::on_netlink_event(ev); });
+			[&netlink_socket](auto const& ev) { ::on_netlink_event(ev, netlink_socket); });
 
 	try {
 		do {
 			poller->wait();
 		} while (true);
-	} catch (...) {
-		throw_backtrace();
+	} catch (std::exception const& e) {
+		::backtrace(e);
+		return 1;
 	}
 
 	return 0;
